7e4fbd3f51676b605bc417355c4a59b79cb034a4
atlanticstarr1 <david.noel.0306@gmail.com>
2021-11-07 23:40:34 -0500

    Updates

---
+++ precache-manifest.82641e4a87802e1d2e8d608f37d101af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0d94eabaa05cd3f592beedc32d9d07",
+    "url": "/cryptobankapp/index.html"
+  },
+  {
+    "revision": "977eb4a7bc3eb6390654",
+    "url": "/cryptobankapp/static/js/2.94394cd4.chunk.js"
+  },
+  {
+    "revision": "bb358c5dae9ddb090dd420b8da612404",
+    "url": "/cryptobankapp/static/js/2.94394cd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd16107ea3ccca18fd3",
+    "url": "/cryptobankapp/static/js/main.7a373e39.chunk.js"
+  },
+  {
+    "revision": "5a1de79e3872dea10561",
+    "url": "/cryptobankapp/static/js/runtime-main.597db1aa.js"
+  },
+  {
+    "revision": "7cb25c30fdac7ebd409b89def2f639f2",
+    "url": "/cryptobankapp/static/media/usdc.7cb25c30.svg"
+  },
+  {
+    "revision": "f9b448835948ecd3ee8a22200875e30a",
+    "url": "/cryptobankapp/static/media/wbtc.f9b44883.svg"
+  }
+]);
